fe20d7ca3f759e9f7db1f5104bc08c169b7cf7db
Juan Felipe Ortiz Niño <jfelipeon@outlook.com>
2017-10-11 11:14:14 -0500

    Starting js

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 
-(function () {
-  // code
+var ModuloControl = (function () {
+    var privateMethod = function () {
+        // do something
+    };
 })();
